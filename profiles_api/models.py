9c3df63b22c5180b1461b8f0ae776da59c91dd3e
Vladimir <vladimirpismenskii@gmail.com>
2024-10-14 07:59:49 +0500

    feat: added UserProfileManager/AUTH_USER_MODE in settings.py

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,8 +1,37 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
 )
 from django.db import models
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for User profiles."""
+
+    def create_user(self, email, name, password=None):
+        """Create a new User profile."""
+
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser."""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
