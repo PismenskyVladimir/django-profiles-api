e96b5207562008daaf9c42648fc27fe32dc99f06
Vladimir <vladimirpismenskii@gmail.com>
2024-10-17 12:40:33 +0500

    feat: Add UserProfileSerializer()

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView."""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(ModelSerializer):
+    """Serializes a user profile object."""
+
+    def create(self, validated_data):
+        """Create and return a new user."""
+
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'],
+        )
+
+        return user
+
+    class Meta:
+        model = models.UserProfile
+        fields = [
+            'id',
+            'email',
+            'name',
+            'password',
+        ]
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'},
+            },
+        }
