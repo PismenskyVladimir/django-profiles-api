93a547ab2cfc708dfa4ade0ac04898bcb0ca2b6a
Vladimir <vladimirpismenskii@gmail.com>
2024-10-19 21:26:25 +0500

    feat: Add ProfileFeedItem()

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from profiles_api.models import UserProfile
+from profiles_api.models import (
+    ProfileFeedItem,
+    UserProfile,
+)
 
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -55,3 +56,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status."""
+
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
